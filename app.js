bffdad77b884c1e3153986a54a6b12564b841da0
edwinchen85 <edwinchen85@gmail.com>
2017-04-08 11:13:10 +0800

    Set up other keys we will need for our Vue instance

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 new Vue({
   // Target the div with an ID of 'events'
-  el: '#events'
+  el: '#events',
+
+  // Here we can register any values or collections that hold data
+  // for the application
+  data: {},
+
+  // Anything within the ready function will run when appliction loads
+  ready: function() {},
+
+  // Methods we want to use in our application are registered here
+  methods: {}
 });
